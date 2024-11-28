a16a693a9b3e42506e3560d10d9ca31aeae3c1d4
Sok-Pich <sokpich@lomatechnology.com>
2024-11-28 16:57:33 +0700

    Update ContentView.swift
    add "Monaco" to Country Array

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US", "Monaco"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
