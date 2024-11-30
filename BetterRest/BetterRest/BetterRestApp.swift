152bf5dec27fc7b01be637d7adbfd775fa39c9fa
Sok-Pich <sokpich@lomatechnology.com>
2024-11-30 12:13:31 +0700

    30th Nov 2024

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sok Pich on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by Sok Pich on 11/30/24.
+//
+
+import Testing
+@testable import BetterRest
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
