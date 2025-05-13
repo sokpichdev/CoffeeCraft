b9bfa726e1b4a560ff339a356baab2c31b786216
Sok-Pich <sokpich@lomatechnology.com>
2025-05-13 17:26:48 +0700

    add test app

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sok Pich on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
