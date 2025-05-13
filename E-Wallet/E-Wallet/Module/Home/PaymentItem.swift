205c5b1b9ca0b2b96bd55d218efa2e68eb089444
Sok-Pich <sokpich@lomatechnology.com>
2025-05-13 16:44:02 +0700

    E-Wallet

---
+++ E-Wallet/E-Wallet/Module/Home/PaymentItem.swift
@@ -0,0 +1,14 @@
+//
+//  PaymentItem.swift
+//  E-Wallet
+//
+//  Created by Sok Pich on 5/13/25.
+//
+
+import SwiftUI
+
+struct PaymentItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let imageUrl: String
+}
