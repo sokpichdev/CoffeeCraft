98e90317c1d8ed91d1c056ea9c93ed716c2b8721
Sok-Pich <sokpich@lomatechnology.com>
2025-05-13 17:18:16 +0700

    E-wallet

--- E-Wallet/E-Wallet/E_WalletApp.swift
+++ E-Wallet/E-Wallet/E_WalletApp.swift
@@ -1,6 +1,6 @@
 //
-//  AnimationApp.swift
-//  Animation
+//  E_WalletApp.swift
+//  E-Wallet
 //
 //  Created by Sok Pich on 5/13/25.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct AnimationApp: App {
+struct E_WalletApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
