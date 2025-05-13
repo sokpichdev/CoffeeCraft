ef028091b24f92ac5740766102ff8cb4fe80adb2
Sok-Pich <sokpich@lomatechnology.com>
2025-05-13 17:56:39 +0700

    new app

---
+++ MyWallet/MyWallet/MyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWalletApp.swift
+//  MyWallet
+//
+//  Created by Sok Pich on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
