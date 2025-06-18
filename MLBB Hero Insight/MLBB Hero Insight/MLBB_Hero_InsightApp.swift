814898aa2762eacecd2ba7feaa87d45f052d9f60
Sok-Pich <sokpich@lomatechnology.com>
2025-06-18 11:51:01 +0700

    set up project

---
+++ MLBB Hero Insight/MLBB Hero Insight/MLBB_Hero_InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLBB_Hero_InsightApp.swift
+//  MLBB Hero Insight
+//
+//  Created by Sok Pich on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MLBB_Hero_InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
