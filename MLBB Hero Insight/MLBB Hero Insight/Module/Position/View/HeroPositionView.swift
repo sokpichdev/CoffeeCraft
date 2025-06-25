ea10f951100b1851918635b5aea0444204460c53
Sok-Pich <sokpich@lomatechnology.com>
2025-06-25 18:49:01 +0700

    added tool bar for filtering.

--- MLBB Hero Insight/MLBB Hero Insight/Module/Position/View/HeroPositionView.swift
+++ MLBB Hero Insight/MLBB Hero Insight/Module/Position/View/HeroPositionView.swift
@@ -53,6 +53,26 @@ struct HeroPositionView: View {
             }
             .navigationTitle("Hero Positions")
             .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showFilterSheet = true
+                    } label: {
+                        ZStack(alignment: .topTrailing) {
+                            Image(systemName: "line.3.horizontal.decrease.circle")
+                                .font(.title3)
+                            
+                            if viewModel.isBeingFiltered {
+                                Circle()
+                                    .fill(Color.red)
+                                    .frame(width: 8, height: 8)
+                                    .offset(x: 6, y: -6)
+                            }
+                        }
+                        .accessibilityLabel("Filter")
+                    }
+                }
+            }
             .onAppear {
                 Task { await viewModel.loadPosition()}
             }
