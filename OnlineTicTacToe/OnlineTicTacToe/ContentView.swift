e479b562260a5a68d8b67b4fbfa3a7f0c7f4fe3b
Sok-Pich <sokpich@lomatechnology.com>
2025-08-15 10:29:04 +0700

    Created a new project called OnlineTicTacToe

---
+++ OnlineTicTacToe/OnlineTicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineTicTacToe
+//
+//  Created by Sok Pich on 8/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineTicTacToe/OnlineTicTacToe/OnlineTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineTicTacToeApp.swift
+//  OnlineTicTacToe
+//
+//  Created by Sok Pich on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct OnlineTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
