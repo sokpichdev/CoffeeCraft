644dc2e91da97deb18fc2bcfd5565a8447b13457
Sok-Pich <sokpich@lomatechnology.com>
2026-01-16 14:27:21 +0700

    Implemented FavoriteView to display favorite products

---
+++ CoffeeCraft/CoffeeCraft/Module/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  CoffeeCraft
+//
+//  Created by Sok Pich on 1/16/26.
+//
+
+import SwiftUI
+
+class MainViewModel: ObservableObject {
+//    @Published var selectedTab: Tab = .home
+}
