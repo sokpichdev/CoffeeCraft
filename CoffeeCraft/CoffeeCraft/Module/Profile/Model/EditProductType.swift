42cb4030de3e193d74420fa3112c760359c04c57
Sok-Pich <sokpich@lomatechnology.com>
2026-01-19 17:48:58 +0700

    Enhance UI for Profile Screen

---
+++ CoffeeCraft/CoffeeCraft/Module/Profile/Model/EditProductType.swift
@@ -0,0 +1,15 @@
+//
+//  EditProductType.swift
+//  CoffeeCraft
+//
+//  Created by Sok Pich on 1/19/26.
+//
+import SwiftUI
+
+enum EditProductType {
+    case name
+    case phone
+    case email // no edit allowed
+    case date
+    case dropDown
+}
