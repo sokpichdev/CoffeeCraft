eac2d2bc2ccbae171b757fe0b8814feb27ba44ac
Sok-Pich <sokpich@lomatechnology.com>
2026-01-26 16:28:42 +0700

    implemented add card view

---
+++ CoffeeCraft/CoffeeCraft/Module/Account/Puchase-Cards/Model/AlertMessage.swift
@@ -0,0 +1,12 @@
+//
+//  AlertMessage.swift
+//  CoffeeCraft
+//
+//  Created by Sok Pich on 1/26/26.
+//
+import SwiftUI
+
+struct AlertMessage: Identifiable {
+    let id = UUID()
+    let message: String
+}
