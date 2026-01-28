00fe26af21862fabc933958b81e54232dc567160
Sok-Pich <sokpich@lomatechnology.com>
2026-01-28 17:08:22 +0700

    remove detent from share card view

--- CoffeeCraft/CoffeeCraft/Module/Account/Puchase-Cards/View/ShareCardSheet.swift
+++ CoffeeCraft/CoffeeCraft/Module/Account/Puchase-Cards/View/ShareCardSheet.swift
@@ -86,7 +86,6 @@ struct ShareCardSheet: View {
                 }
             }
         }
-        .presentationDetents([.medium, .large])
     }
     
     private func shareCard(userID: String) {
