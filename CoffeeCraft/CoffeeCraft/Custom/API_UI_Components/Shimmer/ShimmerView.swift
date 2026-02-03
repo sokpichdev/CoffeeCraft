25f79d74f7bf61a704d89030b9eddcf8bad2cc27
Sok-Pich <sokpich@lomatechnology.com>
2026-02-03 15:19:38 +0700

    Create ShimmerView.swift

---
+++ CoffeeCraft/CoffeeCraft/Custom/API_UI_Components/Shimmer/ShimmerView.swift
@@ -0,0 +1,29 @@
+//
+//  ShimmerView.swift
+//  CoffeeCraft
+//
+//  Created by Sok Pich on 2/3/26.
+//
+import SwiftUI
+
+struct ShimmerView: View {
+    private let gradient = Gradient(colors: [
+        Color(hex: "6F4E37").opacity(0.3), // base brown
+        Color(hex: "D3B8AE").opacity(0.6), // lighter shimmer
+        Color(hex: "6F4E37").opacity(0.3)
+    ])
+    
+    var body: some View {
+        TimelineView(.animation(minimumInterval: 1/60, paused: false)) { timeline in
+            let date = timeline.date.timeIntervalSinceReferenceDate
+            let offset = date.truncatingRemainder(dividingBy: 2) / 2 // range: 0 -> 1
+            
+            LinearGradient(
+                gradient: gradient,
+                startPoint: .init(x: -1 + offset * 2, y: 0.5),
+                endPoint: .init(x: offset * 2, y: 0.5)
+            )
+            .cornerRadius(Dimens.cornerRadius)
+        }
+    }
+}
