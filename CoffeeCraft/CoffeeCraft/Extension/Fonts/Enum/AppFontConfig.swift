f9c4fab8cd106b67b5e6aee1b24281b7a305bd9a
Sok-Pich <sokpich@lomatechnology.com>
2026-02-03 12:12:43 +0700

    added font LibreBaskerVille and PlayfairDisplay

---
+++ CoffeeCraft/CoffeeCraft/Extension/Fonts/Enum/AppFontConfig.swift
@@ -0,0 +1,78 @@
+//
+//  AppFontConfig.swift
+//  CoffeeCraft
+//
+//  Created by Sok Pich on 2/3/26.
+//
+
+
+import SwiftUI
+import UIKit
+
+enum AppFontConfig {
+    
+    /// Change this ONE line to switch the entire app font
+    static var primary: AppFont = .nunito
+}
+
+// MARK: - App Font Weight
+
+enum AppFontWeight {
+    case black
+    case light
+    case regular
+    case medium
+    case semibold
+    case bold
+    
+    var suffix: String {
+        switch self {
+        case .regular:  return "Regular"
+        case .medium:   return "Medium"
+        case .semibold: return "SemiBold"
+        case .bold:     return "Bold"
+        case .black: return "Black"
+        case .light:  return "Light"
+        }
+    }
+    
+    /// Map to SwiftUI Font.Weight for SF Pro fallback
+    var systemWeight: Font.Weight {
+        switch self {
+        case .regular:  return .regular
+        case .medium:   return .medium
+        case .semibold: return .semibold
+        case .bold:     return .bold
+        case .black: return .black
+        case .light: return .light
+        }
+    }
+}
+
+// MARK: - App Font Family
+
+enum AppFont {
+    case nokora
+    case roboto
+    case poppins
+    case montserrat
+    case nunito
+    case playfairDisplay
+    case LibreBaskerville
+    
+    private var family: String {
+        switch self {
+        case .nokora:      return "Nokora"
+        case .roboto:      return "Roboto"
+        case .poppins:     return "Poppins"
+        case .montserrat:  return "Montserrat"
+        case .nunito:      return "Nunito"
+        case .playfairDisplay: return "PlayfairDisplay"
+        case .LibreBaskerville: return "LibreBaskerville"
+        }
+    }
+    
+    func name(for weight: AppFontWeight) -> String {
+        "\(family)-\(weight.suffix)"
+    }
+}
