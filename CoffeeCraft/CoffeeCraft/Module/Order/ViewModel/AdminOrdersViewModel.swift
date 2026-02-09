55dd347894164dfe81731bbd71c44265cde5216c
Sok-Pich <sokpich@lomatechnology.com>
2026-02-09 19:17:52 +0700

    Revert "update the ordering sorting to display the oldest on top."
    This reverts commit 456cdcdf22e77d4a5a6dcaf2f73b88953fc03ea3.

--- CoffeeCraft/CoffeeCraft/Module/Order/ViewModel/AdminOrdersViewModel.swift
+++ CoffeeCraft/CoffeeCraft/Module/Order/ViewModel/AdminOrdersViewModel.swift
@@ -40,7 +40,7 @@ class AdminOrdersViewModel: ObservableObject {
         
         // Fetch paginated data
         db.collection("orders")
-            .order(by: "timestamp", descending: false)
+            .order(by: "timestamp", descending: true)
             .getDocuments { [weak self] snapshot, error in
                 guard let self = self else {
                     completion?(false)
@@ -96,8 +96,8 @@ class AdminOrdersViewModel: ObservableObject {
         allOrdersListener?.remove()
         
         allOrdersListener = db.collection("orders")
-            .order(by: "timestamp", descending: false)
-            .limit(toLast: pageSize) // ensures I still listen to the newest ones, bcuz ordering ascending, new documents arrive at the end
+            .order(by: "timestamp", descending: true)
+            .limit(to: pageSize)
             .addSnapshotListener { [weak self] snapshot, error in
                 guard let self = self else { return }
                 guard let changes = snapshot?.documentChanges else { return }
@@ -107,7 +107,7 @@ class AdminOrdersViewModel: ObservableObject {
                         if let newOrder = try? change.document.data(as: Order.self) {
                             // Add to beginning if not already present
                             if !self.allOrders.contains(where: { $0.id == newOrder.id }) {
-                                self.allOrders.append(newOrder) // oldest first
+                                self.allOrders.insert(newOrder, at: 0)
                                 self.totalAllOrdersCount += 1
                             }
                         }
