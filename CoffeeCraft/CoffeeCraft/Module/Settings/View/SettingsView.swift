e1618ab4987a37c749984715804569f3c493c5dc
Sok-Pich <sokpich@lomatechnology.com>
2026-02-25 18:43:32 +0700

    Changed Button if user not logged in
    switch between "Logout" and "Login" depend of UserSession.shared.isLoggedIn

--- CoffeeCraft/CoffeeCraft/Module/Settings/View/SettingsView.swift
+++ CoffeeCraft/CoffeeCraft/Module/Settings/View/SettingsView.swift
@@ -45,20 +45,27 @@ struct SettingsView: View {
                     RowInSectionView(title: "Write a Review", systemImage: "star.fill")
                 }
                 
-                CustomCoffeeButton(title: "Logout", buttonImage: "rectangle.portrait.and.arrow.right.fill", bgColors: [Color.red, Color.red.opacity(0.85)], contentPlacement: .leading) {
-                    AlertManager.shared.showDestructive(
-                        title: "Log Out?",
-                        message: "Are you sure you want to log out?",
-                        destructiveTitle: "Log Out"
-                    ) {
-                        authVM.logout { isSuccess in
-                            if isSuccess {
-                                dismiss()
+                if UserSession.shared.isLoggedIn {
+                    CustomCoffeeButton(title: "Logout", buttonImage: "rectangle.portrait.and.arrow.right.fill", bgColors: [Color.errorRed, Color.errorRed.opacity(0.85)], contentPlacement: .leading) {
+                        AlertManager.shared.showDestructive(
+                            title: "Log Out?",
+                            message: "Are you sure you want to log out?",
+                            destructiveTitle: "Log Out"
+                        ) {
+                            authVM.logout { isSuccess in
+                                if isSuccess {
+                                    dismiss()
+                                }
                             }
                         }
                     }
+                    .padding(.top, 8)
+                } else {
+                    CustomCoffeeButton(title: "Log In", buttonImage: "rectangle.portrait.and.arrow.right.fill", bgColors: [Color.leafGreen, Color.leafGreen.opacity(0.85)], contentPlacement: .leading) {
+                        push(AnyView(AuthView().environmentObject(authVM)))
+                    }
+                    .padding(.top, 8)
                 }
-                .padding(.top, 8)
                 
                 Text("Version 1.0.0")
                     .font(.caption)
